8b990bfd0497ac2e561f5a97a78b1e8f79f4b71a
SylvainNogrette <sylvain.nogrette@live.fr>
2025-01-16 12:17:33 +0100

    adding function dl img and debug buildBook

--- scrap.py
+++ scrap.py
@@ -2,7 +2,10 @@ import requests
 import csv
 import os
 from bs4 import BeautifulSoup
-from recup_prodParCategory import prodParCategory
+import urllib.request
+import re
+
+urlAccueil = "http://books.toscrape.com/"
 def BuiltDirectoryCategory(cat):
     try :
         os.mkdir(cat)
@@ -18,7 +21,7 @@ def GetBooksFromCategory(url):
         soupbook = soup.findAll("h3")
         try : 
             for article in soupbook:
-                listbook.append(article.find("a").get("href"))
+                listbook.append(urlAccueil + "catalogue/"+article.find("a").get("href").lstrip("./"))
         except Exception as e :
             print(e)    
     return listbook
@@ -44,27 +47,27 @@ def GetSoup(url):
         print ("Page inexistante")
 
 
-def BuiltBook(urlbook):
+def BuiltBook(cat,urlbook):
     soup = GetSoup(urlbook)
     try :
-        listinfo = {}
-        listinfo["product_page_url"] = urlbook
-        listinfo["universal_product_code"] = ""
-        listinfo["title"] = ""
-        listinfo["price_including_tax"] = ""
-        listinfo["price_excluding_tax"] = ""
-        listinfo["number_available"] = ""
-        listinfo["product_description"] = ""
-        listinfo["category"] = ""
-        listinfo["review_rating"] = ""
-        listinfo["image_url"] = ""
-        
 
+
+        bookInfos = []
+        bookInfos.append(urlbook)   # product_page_url
+        bookInfos.append(soup.findAll("tr")[0].find("td").text) # universal_product_code
+        bookInfos.append(soup.find("h1").text)  # title
+        bookInfos.append(soup.findAll("tr")[3].find("td").text.strip("Â")) # price_including_tax
+        bookInfos.append(soup.findAll("tr")[2].find("td").text.strip("Â")) # price_excluding_tax
+        bookInfos.append(soup.findAll("tr")[5].find("td").text) # number_available
+        bookInfos.append(soup.find("meta",{"name":"description"}).get("content").strip())   # product_description
+        bookInfos.append(cat)   # category
+        bookInfos.append(soup.find("div",{"class":"col-sm-6 product_main"}).findAll("p")[2].attrs['class'][1])  # review_rating
+        bookInfos.append(urlAccueil + soup.find("img").get("src").lstrip('./')) # image_url
     
     except Exception as e :
         print(e)
+    return bookInfos
 
-urlAccueil = "http://books.toscrape.com/"
 response = requests.get(urlAccueil)
 if response.status_code == 200:
     htmlAccueil = response.text
@@ -83,11 +86,19 @@ if response.status_code == 200:
                 tempdict.append(a.text.strip())
                 tempdict.append(urlcategory)
             categoriesList.append(tempdict)
-        # listEverybooks =[]
-        # for cat , url in categoriesList :
-        #     for elt in GetBooksFromCategory(url):
-        #         listEverybooks.append(elt)
-        # print(len(listEverybooks))
+        headers = ["product_page_url","universal_product_code","title","price_including_tax","price_excluding_tax","number_available","product_description","category","review_rating","image_url"]
+        for category , url in categoriesList :
+            BuiltDirectoryCategory(category)
+            os.chdir(category)
+            with open(str(category)+"_books.csv","w") as fichier_csv :
+                writer = csv.writer(fichier_csv,delimiter=',')
+                writer.writerow(headers)
+                for bookUrl in GetBooksFromCategory(url):
+                    BuiltBookInfo = BuiltBook(category,bookUrl)
+                    writer.writerow(BuiltBookInfo)
+                    urllib.request.urlretrieve(BuiltBookInfo[-1], re.sub(r'[\\/*?:"<>|]',"",str(BuiltBookInfo[2]))+".jpg")
+            os.chdir('..')
+        
 
     
     except Exception as e: 
@@ -96,12 +107,7 @@ if response.status_code == 200:
 else :
         print("Echec : la page n'a pas était retrouvée")
 
-for cat, url in categoriesList :
-       BuiltDirectoryCategory(cat)
-
-# ###Créer les directories
-# lister les urls des livres de la categorie
-# changer le dir courant sur celui de la cat
+# for cat, url in ca
 # créer csv 
 # récup info livre de la cat
 #  ecrire dans le csv les infos de chaque livre
