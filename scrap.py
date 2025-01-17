4443b5912d39e870f42ab9d23365f6f02bd56bc9
SylvainNogrette <sylvain.nogrette@live.fr>
2025-01-17 21:04:14 +0100

    Exchange append method with extend, a minor changes for better readability

--- scrap.py
+++ scrap.py
@@ -5,7 +5,10 @@ from bs4 import BeautifulSoup
 import urllib.request
 import re
 
+#Global variables :
 urlAccueil = "http://books.toscrape.com/"
+
+
 def BuiltDirectoryCategory(cat):
     try :
         os.mkdir(cat)
@@ -14,6 +17,8 @@ def BuiltDirectoryCategory(cat):
     os.chdir(cat)          
     os.chdir('..')
     return
+
+
 def GetBooksFromCategory(url):
     listbook = []
     for urlpage in GetAllUrlPagesFromCategories(url) :
@@ -21,22 +26,30 @@ def GetBooksFromCategory(url):
         soupbook = soup.findAll("h3")
         try : 
             for article in soupbook:
-                listbook.append(urlAccueil + "catalogue/"+article.find("a").get("href").lstrip("./"))
+                listbook.append(
+                      urlAccueil 
+                    + "catalogue/"+article.find("a").get("href").lstrip("./")
+                    )
         except Exception as e :
             print(e)    
     return listbook
+
+
 def GetAllUrlPagesFromCategories(firsturl):
     soup = GetSoup(firsturl)
     urlPagesFromCategories = [firsturl]
     i=1
     try :
         while soup.find("li",{"class" : "next"}) != None:
-            urlPagesFromCategories.append(firsturl.rstrip("index.html") + soup.find("li",{"class" : "next"}).find("a").get("href"))
+            urlPagesFromCategories.append(
+                firsturl.rstrip("index.html") 
+                + soup.find("li",{"class" : "next"}).find("a").get("href"))
             soup = GetSoup(urlPagesFromCategories[i])
             i+=1
         return urlPagesFromCategories
     except Exception as e :
         print (e)
+
 
 def GetSoup(url):
     response = requests.get(url)
@@ -50,19 +63,31 @@ def GetSoup(url):
 def BuiltBook(cat,urlbook):
     soup = GetSoup(urlbook)
     try :
-
-
+        
+        # Contenu de bookInfos correspondant au en-tête du fichier csv :
+        # product_page_url
+        # universal_product_code
+        # title
+        # price_including_tax
+        # price_excluding_tax
+        # number_available
+        # product_description
+        # category
+        # review_rating
+        # image_url
+        
         bookInfos = []
-        bookInfos.append(urlbook)   # product_page_url
-        bookInfos.append(soup.findAll("tr")[0].find("td").text) # universal_product_code
-        bookInfos.append(soup.find("h1").text)  # title
-        bookInfos.append(soup.findAll("tr")[3].find("td").text.strip("Â")) # price_including_tax
-        bookInfos.append(soup.findAll("tr")[2].find("td").text.strip("Â")) # price_excluding_tax
-        bookInfos.append(soup.findAll("tr")[5].find("td").text) # number_available
-        bookInfos.append(soup.find("meta",{"name":"description"}).get("content").strip())   # product_description
-        bookInfos.append(cat)   # category
-        bookInfos.append(soup.find("div",{"class":"col-sm-6 product_main"}).findAll("p")[2].attrs['class'][1])  # review_rating
-        bookInfos.append(urlAccueil + soup.find("img").get("src").lstrip('./')) # image_url
+        bookInfos.extend([
+            urlbook,                                                                              # product_page_url
+            soup.findAll("tr")[0].find("td").text,                                                # universal_product_code
+            soup.find("h1").text,                                                                 # title
+            soup.findAll("tr")[3].find("td").text.strip("Â"),                                     # price_including_tax
+            soup.findAll("tr")[2].find("td").text.strip("Â"),                                     # price_excluding_tax
+            soup.findAll("tr")[5].find("td").text,                                                # number_available
+            soup.find("meta",{"name":"description"}).get("content").strip(),                      # product_description
+            cat,                                                                                  # category
+            soup.find("div",{"class":"col-sm-6 product_main"}).findAll("p")[2].attrs['class'][1], # review_rating
+            urlAccueil + soup.find("img").get("src").lstrip('./')])                               # image_url
     
     except Exception as e :
         print(e)
@@ -76,6 +101,7 @@ if response.status_code == 200:
     try : 
         i=0
         categoriesList= []
+
         for a in ulcategories.findAll("a"):
             tempdict = []
             if i == 0 :
@@ -86,7 +112,12 @@ if response.status_code == 200:
                 tempdict.append(a.text.strip())
                 tempdict.append(urlcategory)
             categoriesList.append(tempdict)
-        headers = ["product_page_url","universal_product_code","title","price_including_tax","price_excluding_tax","number_available","product_description","category","review_rating","image_url"]
+        
+        headers = [
+            "product_page_url","universal_product_code","title",
+            "price_including_tax","price_excluding_tax",
+            "number_available","product_description","category",
+            "review_rating","image_url"]
         for category , url in categoriesList :
             BuiltDirectoryCategory(category)
             os.chdir(category)
@@ -96,7 +127,9 @@ if response.status_code == 200:
                 for bookUrl in GetBooksFromCategory(url):
                     BuiltBookInfo = BuiltBook(category,bookUrl)
                     writer.writerow(BuiltBookInfo)
-                    urllib.request.urlretrieve(BuiltBookInfo[-1], re.sub(r'[\\/*?:"<>|]',"",str(BuiltBookInfo[2]))+".jpg")
+                    urllib.request.urlretrieve(
+                        BuiltBookInfo[-1], re.sub(r'[\\/*?:"<>|]',"",str(BuiltBookInfo[2]))
+                        +".jpg")
             os.chdir('..')
         
 
